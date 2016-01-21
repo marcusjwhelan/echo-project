155f1e098d405ad0ae31da8690868b340d553e16
Marcus <marcus.j.whelan@gmail.com>
2016-01-21 03:13:22 +0000

    Echo-project online when used on cloud9

---
+++ echo-project-work/echo-project-work.js
@@ -0,0 +1,44 @@
+
+
+if(Meteor.isClient){
+Meteor.startup(function(){
+  WebFontConfig = {
+    google: { families: [ 'Droid+Sans::latin', 'Orbitron::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+    console.log("async fonts loaded", WebFontConfig);
+  })();
+});//end of Meteor.startup
+};// end of is client
+/*
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+*/
