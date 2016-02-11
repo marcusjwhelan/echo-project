d5c54028a082f9f9a8284c7f597a37bf2e1e9b38
Marcus <marcus.j.whelan@gmail.com>
2016-02-11 06:58:24 +0000

    2.11.16 added node.name

--- echo-project-work/client/templates/nodes/graph.js
+++ echo-project-work/client/templates/nodes/graph.js
@@ -1,10 +1,21 @@
-//Example given by Stephen Woods. Stackoverflow answer to my question.
 Template.graph.helpers({
   humid: function() {
     if( Router.current().params._id ) {
       let node = Nodes.findOne({ _id: Router.current().params._id });
       if( node ) {
-        return node.humidity;
+        var humidityL = [];
+        for(i=0; i<node.humidity.length;i++){
+          humidityL.push(node.humidity[i]);
+        }
+        return humidityL;
+      }
+    }
+  },
+  name: function(){
+    if(Router.current().params._id){
+      let node = Nodes.findOne({_id: Router.current().params._id});
+      if(node){
+        return node.name;
       }
     }
   }
