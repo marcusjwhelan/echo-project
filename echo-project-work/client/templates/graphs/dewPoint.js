1791f384215237e23d78765de38f9809342d4bef
Marcus <marcus.j.whelan@gmail.com>
2016-02-18 06:05:53 +0000

    2.18.16 Spline working

--- echo-project-work/client/templates/graphs/dewPoint.js
+++ echo-project-work/client/templates/graphs/dewPoint.js
@@ -56,7 +56,7 @@ function lineChart5(){
           }
         }
         chart5 = $('#line-chart-container5').highcharts({
-          title: {
+          /*title: {
             text: ' ',
             x: -20 // center?
           },
@@ -65,7 +65,7 @@ function lineChart5(){
           },
           yAxis: {
             title: {
-              text: 'Degrees Fahrenheit'
+              text: 'Dew Point'
             },
             plotLines:[{
               value: 0,
@@ -74,7 +74,7 @@ function lineChart5(){
             }]
           },
           tooltip:{
-            valueSuffix: ' + F'
+            valueSuffix: ' + %'
           },
           legend: {
             layout: 'vertical',
@@ -85,15 +85,86 @@ function lineChart5(){
           series: [{
             name: 'dew point',
             data: data
-          }/*if you have more than one plot
-          on the graph add a 
-          {
-            name: 'a;dslkj',
-            data: ;;lkj
-          },*/],
+          }
+          *if you have more than one plot
+          *on the graph add a 
+          *{
+          *  name: 'a;dslkj',
+          *  data: ;;lkj
+          *},//],
           credits: {
             enabled: false
-          }
+          }*/
+          
+          
+          /*All Right. Copy and paste here but it works. Now to figure out how
+          this works and get my data to load on here.*/
+          chart: {
+                type: 'spline',
+                animation: Highcharts.svg, // don't animate in old IE
+                marginRight: 10,
+                events: {
+                    load: function () {
+
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        setInterval(function () {
+                            var x = (new Date()).getTime(), // current time
+                                y = Math.random();
+                            series.addPoint([x, y], true, true);
+                        }, 1000);
+                    }
+                }
+            },
+            title: {
+                text: 'Live random data'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                        Highcharts.numberFormat(this.y, 2);
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            series: [{
+                name: 'Random data',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -19; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.random()
+                        });
+                    }
+                    return data;
+                }())
+            }]
+          /*----------------------------------------------------*/
+          
         });
       }
     }
@@ -108,7 +179,7 @@ Template.dewPoint.rendered = function(){
 Template.dewPoint.events({
   // add new random value to this particular node
   'click #add-value5': function(){
-    var rand = Math.floor((Math.random()*(100))-40);
+    var rand = Math.floor((Math.random()*(100))+ 0);
     if(Router.current().params._id){
       let node = Nodes.findOne({_id: Router.current().params._id});
       if(node){
