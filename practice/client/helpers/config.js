7a1799d978d6ef098cc4e401c1c86b7080a62669
Marcus <marcus.j.whelan@gmail.com>
2016-03-07 18:56:43 +0000

    deleted old folders

--- practice/client/helpers/config.js
+++
@@ -1,6 +0,0 @@
-// To tell our accounts system that we want users 
-// to log-in via a username, we simple add and
-// Accounts.ui config block in /clients/helpers.
-Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_ONLY'
-});
